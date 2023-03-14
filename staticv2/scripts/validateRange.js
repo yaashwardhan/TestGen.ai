07a5a4b5cdb9e46c7401047e95791124d7c11124
Yashwardhan Deshmukh <yaashwardhan@gmail.com>
2023-03-14 08:29:58 +0530

    Delete staticv2 directory

--- staticv2/scripts/validateRange.js
+++
@@ -1,8 +0,0 @@
-function validateRange() {
-    var rangeValue = document.getElementById("question-count").value;
-    if (rangeValue == 0) {
-      alert("Please choose a question count greater than 0.");
-      return false; // Prevent form submission
-    }
-    return true; // Allow form submission
-  }
